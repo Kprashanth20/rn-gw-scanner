d6c21cb076311bfa9bdb2a7da474b00f1ee72219
Otmane <otmane@godamwale.com>
2022-04-22 19:00:30 +0000

    make the view full screen

--- android/src/main/java/com/reactnativerngwscanner/RnGwScannerView.java
+++ android/src/main/java/com/reactnativerngwscanner/RnGwScannerView.java
@@ -1,37 +1,29 @@
 package com.reactnativerngwscanner;
 
-import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.text.TextUtils;
-import android.util.Base64;
-import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.huawei.hms.hmsscankit.RemoteView;
 import com.huawei.hms.ml.scan.HmsScan;
 
-import java.io.ByteArrayOutputStream;
-
 public class RnGwScannerView extends FrameLayout {
-  private int scanType;
-  private boolean continuouslyScan;
-  private boolean enableReturnOriginalScan;
+  private final ThemedReactContext context;
 
-  private RemoteView remoteView;
   private final Gson gson;
 
   public enum Event {
-    RESPONSE("response"),
-    ORIGINAL_SCAN_LOAD("originalScanLoad");
+    RESPONSE("response");
 
     private final String eventName;
 
@@ -44,108 +36,50 @@ public class RnGwScannerView extends FrameLayout {
     }
   }
 
-  public RnGwScannerView(Context context) {
+  public RnGwScannerView(ThemedReactContext context) {
     super(context);
 
+    this.context = context;
     gson = new GsonBuilder().setPrettyPrinting().create();
+
     initView();
   }
 
-  public void setScanType(int scanType) {
-    this.scanType = scanType;
-  }
-
-  public void setContinuouslyScan(boolean continuouslyScan) {
-    this.continuouslyScan = continuouslyScan;
-  }
-
-  public void setEnableReturnOriginalScan(boolean enableReturnOriginalScan) {
-    this.enableReturnOriginalScan = enableReturnOriginalScan;
-  }
-
   private void initView() {
-    inflate(getContext(), R.layout.scanner_view, this);
+    View view = LayoutInflater.from(context).inflate(R.layout.scanner_view, this, true);
+    this.addView(view);
 
-    ImageView scanFrame = findViewById(R.id.scan_area);
+    ImageView scanFrame = view.findViewById(R.id.scan_area);
 
-    DisplayMetrics dm = getResources().getDisplayMetrics();
-    float density = dm.density;
-
-    int mScreenWidth = getResources().getDisplayMetrics().widthPixels;
-    int mScreenHeight = getResources().getDisplayMetrics().heightPixels;
-
-    int scanFrameSizeHeight = (int) (200 * density);
-    int scanFrameSizeWidth = (int) (200 * density);
-
-    Rect rect = new Rect();
-    rect.left = mScreenWidth / 2 - scanFrameSizeWidth / 2;
-    rect.right = mScreenWidth / 2 + scanFrameSizeWidth / 2;
-    rect.top = mScreenHeight / 2 - scanFrameSizeHeight / 2;
-    rect.bottom = mScreenHeight / 2 + scanFrameSizeHeight / 2;
+    Rect rect = new Rect(0, 0, 0, 0);
 
     scanFrame.getLayoutParams().height = rect.height();
     scanFrame.getLayoutParams().width = rect.width();
 
-    Toast.makeText(getContext(), "height " + rect.height() + " width " + rect.width(), Toast.LENGTH_SHORT).show();
+    Toast.makeText(context, "height " + rect.height() + " width " + rect.width(), Toast.LENGTH_SHORT).show();
 
-    RemoteView.Builder builder = new RemoteView.Builder()
-      .setContext(((ReactContext) getContext()).getCurrentActivity())
+    RemoteView remoteView = new RemoteView.Builder()
+      .setContext(context.getCurrentActivity())
       .setBoundingBox(rect)
-      .setFormat(scanType)
-      .setContinuouslyScan(continuouslyScan);
-
-    if (enableReturnOriginalScan) {
-      builder.enableReturnBitmap();
-    }
-
-    remoteView = builder.build();
-    RnGwScannerViewManager.setViews(remoteView);
+      .setContinuouslyScan(false)
+      .setFormat(HmsScan.ALL_SCAN_TYPE)
+      .build();
 
     remoteView.setOnResultCallback(result -> {
-      if (enableReturnOriginalScan) {
-        sendOriginalScan(result[0]);
-      }
-
       if (result != null && result.length > 0 && result[0] != null && !TextUtils.isEmpty(result[0].getOriginalValue())) {
-        if (!continuouslyScan) {
-          HmsScan hmsScan = result[0];
 
-          if (hmsScan != null && !TextUtils.isEmpty(hmsScan.getOriginalValue())) {
-            WritableMap event = Arguments.createMap();
-            event.putString("type", Event.RESPONSE.getName());
-            event.putString("data", gson.toJson(hmsScan));
+        Toast.makeText(context, result[0].getOriginalValue(), Toast.LENGTH_SHORT).show();
 
-            sendEvent("topOnNewScan", event);
-          }
+        WritableMap event = Arguments.createMap();
+        event.putString("type", Event.RESPONSE.getName());
+        event.putString("data", gson.toJson(result[0]));
 
-        } else {
-          WritableMap event = Arguments.createMap();
-          event.putString("type", Event.RESPONSE.getName());
-          event.putString("data", gson.toJson(result[0]));
-
-          sendEvent("topOnNewScan", event);
-        }
+        sendEvent("topOnNewScan", event);
       }
     });
   }
 
-  private void sendOriginalScan(HmsScan scan) {
-    Bitmap bitmap = scan.getOriginalBitmap();
-    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
-    String byteArray = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
-
-    WritableMap event = Arguments.createMap();
-    event.putString("type", Event.ORIGINAL_SCAN_LOAD.getName());
-    event.putString("data", byteArray);
-
-    sendEvent("topOnNewScan", event);
-  }
-
   private void sendEvent(String eventName, WritableMap eventArgs) {
-    ReactContext reactContext = (ReactContext) getContext();
-    reactContext
-      .getJSModule(RCTEventEmitter.class)
-      .receiveEvent(getId(), eventName, eventArgs);
+    context.getJSModule(RCTEventEmitter.class).receiveEvent(getId(), eventName, eventArgs);
   }
 }
--- android/src/main/java/com/reactnativerngwscanner/RnGwScannerViewManager.java
+++ android/src/main/java/com/reactnativerngwscanner/RnGwScannerViewManager.java
@@ -1,23 +1,12 @@
 package com.reactnativerngwscanner;
 
-import android.annotation.SuppressLint;
-import android.widget.ImageView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.collection.ArrayMap;
 
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
-import com.facebook.react.uimanager.annotations.ReactProp;
-import com.huawei.hms.hmsscankit.RemoteView;
-import com.huawei.hms.ml.scan.HmsScan;
 
 import java.util.Map;
 
@@ -25,13 +14,6 @@ public class RnGwScannerViewManager extends ViewGroupManager<RnGwScannerView> {
   public static final String REACT_CLASS = "RnGwScannerView";
 
   private final ReactApplicationContext reactContext;
-
-  @SuppressLint("StaticFieldLeak")
-  private static RemoteView remoteView;
-
-  public static void setViews(RemoteView remoteView) {
-    RnGwScannerViewManager.remoteView = remoteView;
-  }
 
   public RnGwScannerViewManager(ReactApplicationContext reactContext) {
     this.reactContext = reactContext;
@@ -46,52 +28,7 @@ public class RnGwScannerViewManager extends ViewGroupManager<RnGwScannerView> {
   @Override
   @NonNull
   public RnGwScannerView createViewInstance(@NonNull ThemedReactContext reactContext) {
-    Toast.makeText(reactContext, "RnGwScannerView createViewInstance", Toast.LENGTH_SHORT).show();
     return new RnGwScannerView(reactContext);
-  }
-
-  @ReactProp(name = "scanType")
-  public void setScanType(RnGwScannerView view, int scanType) {
-    view.setScanType(scanType);
-  }
-
-  @ReactProp(name = "continuouslyScan", defaultBoolean = false)
-  public void setContinuouslyScan(RnGwScannerView view, boolean continuouslyScan) {
-    view.setContinuouslyScan(continuouslyScan);
-  }
-
-  @ReactProp(name = "enableReturnOriginalScan", defaultBoolean = false)
-  public void setEnableReturnOriginalScan(RnGwScannerView view, boolean enableReturnOriginalScan) {
-    view.setEnableReturnOriginalScan(enableReturnOriginalScan);
-  }
-
-  @ReactMethod
-  public void pauseContinuouslyScan(final Promise promise) {
-    if (remoteView != null) {
-      remoteView.pauseContinuouslyScan();
-      promise.resolve(true);
-    } else {
-      promise.reject("remoteViewError", "Remote View is not initiated");
-    }
-  }
-
-  @ReactMethod
-  public void resumeContinuouslyScan(final Promise promise) {
-    if (remoteView != null) {
-      remoteView.resumeContinuouslyScan();
-      promise.resolve(true);
-    } else {
-      promise.reject("remoteViewError", "Remote View is not initiated");
-    }
-  }
-
-  @ReactMethod
-  public void getLightStatus(final Promise promise) {
-    if (remoteView != null) {
-      promise.resolve(remoteView.getLightStatus());
-    } else {
-      promise.reject("remoteViewError", "Remote View is not initiated");
-    }
   }
 
   @Nullable
@@ -101,33 +38,5 @@ public class RnGwScannerViewManager extends ViewGroupManager<RnGwScannerView> {
       "topOnNewScan",
       MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", "onNewScan"))
     );
-  }
-
-  @Nullable
-  @Override
-  public Map<String, Object> getConstants() {
-    Map<String, Object> constants = new ArrayMap<>();
-    constants.put("SCAN_TYPES", getScanTypes());
-    return constants;
-  }
-
-  public Map<String, Object> getScanTypes() {
-    Map<String, Object> scanTypes = new ArrayMap<>();
-    scanTypes.put("Other", HmsScan.OTHER_FORM);
-    scanTypes.put("All", HmsScan.ALL_SCAN_TYPE);
-    scanTypes.put("Code128", HmsScan.CODE128_SCAN_TYPE);
-    scanTypes.put("Code39", HmsScan.CODE39_SCAN_TYPE);
-    scanTypes.put("Code93", HmsScan.CODE93_SCAN_TYPE);
-    scanTypes.put("Codabar", HmsScan.CODABAR_SCAN_TYPE);
-    scanTypes.put("DataMatrix", HmsScan.DATAMATRIX_SCAN_TYPE);
-    scanTypes.put("EAN13", HmsScan.EAN13_SCAN_TYPE);
-    scanTypes.put("EAN8", HmsScan.EAN8_SCAN_TYPE);
-    scanTypes.put("ITF14", HmsScan.ITF14_SCAN_TYPE);
-    scanTypes.put("QRCode", HmsScan.QRCODE_SCAN_TYPE);
-    scanTypes.put("UPCCodeA", HmsScan.UPCCODE_A_SCAN_TYPE);
-    scanTypes.put("UPCCodeE", HmsScan.UPCCODE_E_SCAN_TYPE);
-    scanTypes.put("Pdf417", HmsScan.PDF417_SCAN_TYPE);
-    scanTypes.put("Aztec", HmsScan.AZTEC_SCAN_TYPE);
-    return scanTypes;
   }
 }
